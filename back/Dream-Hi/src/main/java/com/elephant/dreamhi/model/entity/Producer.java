f745318bf89e05fc00dff00553acafef9439590d
이다운 <innocent61@naver.com>
2023-01-17 15:00:18 +0900

    :sparkles: Feat: Producer Entity 정의

--- back/Dream-Hi/src/main/java/com/elephant/dreamhi/model/entity/Producer.java
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/model/entity/Producer.java
@@ -1,5 +1,27 @@
 package com.elephant.dreamhi.model.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "producer")
 public class Producer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(max = 20)
+    @NotNull
+    private String name;
+
+    @Lob
+    private byte[] description;
 
 }
