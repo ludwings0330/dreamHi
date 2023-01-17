8c8a056c4c21d55cd51f29c17ca0cf599ab879d6
ludwings0330 <ludwings0330@gmail.com>
2023-01-17 12:57:11 +0900

    init

---
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/DreamHiApplication.java
@@ -0,0 +1,13 @@
+package com.elephant.dreamhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamHiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamHiApplication.class, args);
+	}
+
+}
