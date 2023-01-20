064ea2c009389a7a813f4ce0d8cf7a5e306d6a4d
ludwings <81959681+ludwings0330@users.noreply.github.com>
2023-01-20 12:43:18 +0900

    :tada: Init: Response Util 클래스, messages 프로퍼티파일 설정 추가

---
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/utils/Response.java
@@ -0,0 +1,36 @@
+package com.elephant.dreamhi.utils;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class Response {
+
+    private Response() {
+    }
+
+    public static ResponseEntity<Body> create(HttpStatus httpStatus, String message, Object result) {
+
+        return new ResponseEntity<>(
+                Body.builder()
+                    .message(message)
+                    .result(result)
+                    .build(),
+                httpStatus);
+    }
+
+    public static ResponseEntity<Body> ok() {
+        return create(HttpStatus.OK, "ok", null);
+    }
+
+    @Builder
+    @Getter
+    public static class Body {
+
+        private String message;
+        private Object result;
+
+    }
+
+}
