6c210060ccb5d8d038a67f8f6f2c7cbd2dc7f3e3
JunHyxxn <ihrug000@gmail.com>
2023-01-26 22:41:16 +0900

    :sparkles: Feat: PrincipalDetails 구현 완료
    PrincipalDetails : 일반 로그인을 수행할 UserDetails 의 구현체를 구현했습니다

---
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/model/dto/PrincipalDetails.java
@@ -0,0 +1,57 @@
+package com.elephant.dreamhi.model.dto;
+
+import java.util.Collection;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PrincipalDetails implements UserDetails {
+
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private Boolean isNew;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+}
