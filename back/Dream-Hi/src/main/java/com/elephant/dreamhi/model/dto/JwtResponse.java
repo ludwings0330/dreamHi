2867f5688a62f2a20294b80338504eaf4a8d1349
JunHyxxn <ihrug000@gmail.com>
2023-01-26 22:35:53 +0900

    :sparkles: Feat: JWT 관련 Entity, DTO 추가

---
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/model/dto/JwtResponse.java
@@ -0,0 +1,20 @@
+package com.elephant.dreamhi.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JwtResponse {
+
+    private Long id;
+
+    private String accessToken;
+
+    private String refreshToken;
+
+}
