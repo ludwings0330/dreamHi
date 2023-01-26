856feabfc7e49fef60db21cd55debe5ccecc37f3
JunHyxxn <ihrug000@gmail.com>
2023-01-27 00:42:27 +0900

    :sparkles: Feat: OAuth2UserInfo 관련 기능 추가
    - OAuth2UserInfo : 소셜 로그인 interface
    - GoogleOAuth2UserInfo : 구글 로그인 UserInfo
    - NaverOAuth2UserInfo : 네이버 로그인 UserInfo
    - KakaoOAuth2UserInfo : 카카오 로그인 UserInfo
    - ProviderType : Provider Type Enum Class
    - OAuth2UserInfoFactory : Factory

---
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/security/oauth/provider/OAuth2UserInfoFactory.java
@@ -0,0 +1,15 @@
+package com.elephant.dreamhi.security.oauth.provider;
+
+import com.elephant.dreamhi.model.statics.ProviderType;
+import java.util.Map;
+
+public class OAuth2UserInfoFactory {
+    public static OAuth2UserInfo getOAuth2UserInfo(ProviderType providerType, Map<String, Object> attributes) {
+        switch (providerType) {
+            case GOOGLE: return new GoogleOAuth2UserInfo(attributes);
+            case NAVER: return new NaverOAuth2UserInfo(attributes);
+            case KAKAO: return new KakaoOAuth2UserInfo(attributes);
+            default: throw new IllegalArgumentException("Invalid Provider Type.");
+        }
+    }
+}
