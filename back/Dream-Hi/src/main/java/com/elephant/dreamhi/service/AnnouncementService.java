ce6e62c8861d7e349c2081dc3e9813bec82e18c5
DaWoon Lee <qkgk61@gmail.com>
2023-01-30 23:32:45 +0900

    :sparkle: Feat: AnnouncementServiceImpl의 공고 상세 조회 기능 구현 완료

--- back/Dream-Hi/src/main/java/com/elephant/dreamhi/service/AnnouncementService.java
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/service/AnnouncementService.java
@@ -1,9 +1,11 @@
 package com.elephant.dreamhi.service;
 
 import com.elephant.dreamhi.model.dto.AnnouncementDetailDto;
+import com.elephant.dreamhi.security.PrincipalDetails;
+import java.util.Optional;
 
 public interface AnnouncementService {
 
-    AnnouncementDetailDto findDetail(Long id);
+    Optional<AnnouncementDetailDto> findDetail(Long id, PrincipalDetails user);
 
 }
--- back/Dream-Hi/src/main/java/com/elephant/dreamhi/service/AnnouncementServiceImpl.java
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/service/AnnouncementServiceImpl.java
@@ -1,7 +1,10 @@
 package com.elephant.dreamhi.service;
 
 import com.elephant.dreamhi.model.dto.AnnouncementDetailDto;
+import com.elephant.dreamhi.model.entity.Announcement;
 import com.elephant.dreamhi.repository.AnnouncementRepository;
+import com.elephant.dreamhi.security.PrincipalDetails;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,8 +17,12 @@ public class AnnouncementServiceImpl implements AnnouncementService {
     private final AnnouncementRepository announcementRepository;
 
     @Override
-    public AnnouncementDetailDto findDetail(Long id) {
-        return announcementRepository.;
+    public Optional<AnnouncementDetailDto> findDetail(Long announcementId, PrincipalDetails user) {
+        if (!user.getId().equals(0L)) {
+            return announcementRepository.findByAnnouncementIdAndFollowerId(announcementId, user.getId());
+        }
+
+        return announcementRepository.findById(announcementId).map(Announcement::toAnnouncementDetailDto); // fetch join 구현해야 함
     }
 
 }
