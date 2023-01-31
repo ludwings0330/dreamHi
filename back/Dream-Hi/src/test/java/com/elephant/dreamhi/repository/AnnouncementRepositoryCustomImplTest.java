a0bfc7e0befd6c6d1aa0adf99cc1f2fa32b7ffed
이다운 <innocent61@naver.com>
2023-01-31 13:41:47 +0900

    :umbrella_with_rain_drops: Test: AnnouncementRepositoryCustomImpl의 공고 상세 조회 메서드 테스트

---
+++ back/Dream-Hi/src/test/java/com/elephant/dreamhi/repository/AnnouncementRepositoryCustomImplTest.java
@@ -0,0 +1,138 @@
+package com.elephant.dreamhi.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.elephant.dreamhi.config.QueryDslTestConfig;
+import com.elephant.dreamhi.exception.NotFoundException;
+import com.elephant.dreamhi.model.dto.AnnouncementDetailDto;
+import com.elephant.dreamhi.model.entity.Announcement;
+import com.elephant.dreamhi.model.entity.Follow;
+import com.elephant.dreamhi.model.entity.Producer;
+import com.elephant.dreamhi.model.entity.User;
+import com.elephant.dreamhi.model.statics.FollowType;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(QueryDslTestConfig.class)
+class AnnouncementRepositoryCustomImplTest {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Autowired
+    private AnnouncementRepository announcementRepository;
+
+    @Test
+    @DisplayName("로그인 유저가 팔로우하지 않은 공고의 상세 정보를 조회하는 경우")
+    void findByAnnouncementIdAndFollowerId_1() {
+        // given
+        User user = createTestUser();
+        entityManager.persist(user);
+
+        Producer producer = createTestProducer();
+        entityManager.persist(producer);
+
+        Announcement announcement = createTestAnnouncement(producer);
+        entityManager.persist(announcement);
+
+        // when
+        AnnouncementDetailDto finded = announcementRepository.findByAnnouncementIdAndFollowerId(announcement.getId(), user.getId())
+                                                             .orElseThrow(() -> new NotFoundException("DB에 공고가 존재하지 않습니다."));
+
+        entityManager.clear();
+
+        // then
+        assertThat(finded).usingRecursiveComparison()
+                          .isEqualTo(entityManager.find(Announcement.class, announcement.getId()).toAnnouncementDetailDto(Optional.empty()));
+    }
+
+    @Test
+    @DisplayName("로그인 유저가 팔로우한 공고의 상세 정보를 조회하는 경우")
+    void findByAnnouncementIdAndFollowerId_2() {
+        // given
+        User user = createTestUser();
+        entityManager.persist(user);
+
+        Producer producer = createTestProducer();
+        entityManager.persist(producer);
+
+        Announcement announcement = createTestAnnouncement(producer);
+        entityManager.persist(announcement);
+
+        Follow follow = Follow.builder()
+                              .follower(user)
+                              .announcement(announcement)
+                              .type(FollowType.ANNOUNCEMENT)
+                              .build();
+        entityManager.persist(follow);
+
+        // when
+        AnnouncementDetailDto testDto = announcementRepository.findByAnnouncementIdAndFollowerId(announcement.getId(), user.getId())
+                                                              .orElseThrow(() -> new NotFoundException("DB에 공고가 존재하지 않습니다."));
+
+        entityManager.clear();
+
+        // then
+        Optional<Follow> foundFollow = Optional.ofNullable(entityManager.find(Follow.class, follow.getId()));
+
+        assertThat(testDto).usingRecursiveComparison()
+                           .isEqualTo(entityManager.find(Announcement.class, announcement.getId())
+                                                   .toAnnouncementDetailDto(foundFollow));
+    }
+
+    @Test
+    @DisplayName("비로그인 유저가 공고의 상세 정보를 조회하는 경우")
+    void findByAnnouncementIdAndFollowerId_3() {
+        // given
+        Producer producer = createTestProducer();
+        entityManager.persist(producer);
+
+        Announcement announcement = createTestAnnouncement(producer);
+        entityManager.persist(announcement);
+
+        // when
+        AnnouncementDetailDto testDto = announcementRepository.findByAnnouncementIdAndFollowerId(announcement.getId(), 0L)
+                                                              .orElseThrow(() -> new NotFoundException("DB에 공고가 존재하지 않습니다."));
+
+        entityManager.clear();
+
+        // then
+        assertThat(testDto).usingRecursiveComparison()
+                           .isEqualTo(entityManager.find(Announcement.class, announcement.getId())
+                                                   .toAnnouncementDetailDto(Optional.empty()));
+    }
+
+    private Announcement createTestAnnouncement(Producer producer) {
+        return Announcement.builder()
+                           .endDate(LocalDateTime.of(2023, 2, 5, 14, 34, 23))
+                           .title("더미 공고1")
+                           .producer(producer)
+                           .build();
+    }
+
+    private Producer createTestProducer() {
+        return Producer.builder()
+                       .name("더미 제작사")
+                       .build();
+    }
+
+    public User createTestUser() {
+        return User.builder()
+                   .email("dummy@dream.hi")
+                   .name("더미 유저")
+                   .password("123kfd")
+                   .phone("01012346482")
+                   .build();
+    }
+
+}
