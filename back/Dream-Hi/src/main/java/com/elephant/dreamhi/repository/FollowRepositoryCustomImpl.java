8d7ed0ba2ec3b30393512feae5bb60056074e4ca
JunHyxxn <ihrug000@gmail.com>
2023-01-31 14:43:47 +0900

    :hammer: Refactor: 동적 쿼리 코드 수정

--- back/Dream-Hi/src/main/java/com/elephant/dreamhi/repository/FollowRepositoryCustomImpl.java
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/repository/FollowRepositoryCustomImpl.java
@@ -40,12 +40,13 @@ public class FollowRepositoryCustomImpl implements FollowRepositoryCustom {
 
     @Override
     public Optional<Follow> checkFollow(FollowType type, Long id, Long followerId) {
-        JPAQuery<Follow> query = jpaQueryFactory.selectFrom(follow)
-                                                .where(follow.follower.id.eq(followerId));
-
-        query = followFactory(type, id, query);
-
-        return Optional.ofNullable(query.fetchOne());
+        return Optional.ofNullable(jpaQueryFactory.selectFrom(follow)
+                                                .where(
+                                                        follow.follower.id.eq(followerId),
+                                                        actorEq(type, id),
+                                                        producerEq(type, id),
+                                                        announcementEq(type, id)
+                                                ).fetchOne());
     }
 
     @Override
@@ -71,37 +72,6 @@ public class FollowRepositoryCustomImpl implements FollowRepositoryCustom {
 
     private BooleanExpression announcementEq(FollowType type, Long id) {
         return type == FollowType.ANNOUNCEMENT ? follow.announcement.id.eq(id) : null;
-    }
-
-    /**
-     * type에 따라 쿼리문 생성 메소드
-     *
-     * @param type
-     * @param id
-     * @param query
-     * @return JPAQuery<Follow>
-     */
-    private static JPAQuery<Follow> followFactory(FollowType type, Long id, JPAQuery<Follow> query) {
-        switch (type) {
-            case ACTOR:
-                query = query.where(
-                        follow.actor.id.eq(id)
-                );
-                break;
-            case ANNOUNCEMENT:
-                query = query.where(
-                        follow.announcement.id.eq(id)
-                );
-                break;
-            case PRODUCER:
-                query = query.where(
-                        follow.producer.id.eq(id)
-                );
-                break;
-            default:
-                break;
-        }
-        return query;
     }
 
 }
