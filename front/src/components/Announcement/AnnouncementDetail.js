a6959651149e38ad7ba77ae347c12fdc83d07edc
leeyuhmin <yuhmin92@naver.com>
2023-01-31 17:48:32 +0900

    :tractor: Remane: 공고 파일구조 수정

--- front/src/components/Announcement/AnnouncementDetail.js
+++ front/src/components/Announcement/AnnouncementDetail.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function AnnouncementDetailPage(props) {
+function AnnouncementDetail(props) {
   return (
     <div>
       <h1>공고상세페이지</h1>
@@ -9,4 +9,4 @@ function AnnouncementDetailPage(props) {
   );
 }
 
-export default AnnouncementDetailPage;
+export default AnnouncementDetail;
