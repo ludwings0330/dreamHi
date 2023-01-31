d92125d478a89e57bcfeca7e389bf1e1d97b5d54
ludwings0330 <ludwings0330@gmail.com>
2023-01-31 17:19:32 +0900

    :hammer: Refactor: ControllerAdvice DuplicateKeyException 추가

--- back/Dream-Hi/src/main/java/com/elephant/dreamhi/controller/ControllerAdvice.java
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/controller/ControllerAdvice.java
@@ -2,9 +2,9 @@ package com.elephant.dreamhi.controller;
 
 import com.elephant.dreamhi.exception.NotFoundException;
 import com.elephant.dreamhi.utils.Response;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import com.elephant.dreamhi.utils.Response.Body;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,11 +14,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ControllerAdvice {
 
-    @ExceptionHandler({ NotFoundException.class })
-    public ResponseEntity<?> handle404(Exception exception) {
-        log.error("Exception Caused By : {}", exception.getMessage());
-        return Response.create(HttpStatus.NOT_FOUND, exception.getMessage());
+    @ExceptionHandler({ DuplicateKeyException.class })
+    public ResponseEntity<Body> handleConflict(Exception e) {
+        log.error("Exception Caused By : {}", e.getMessage());
+        return Response.create(HttpStatus.CONFLICT, e.getMessage());
     }
 
+    @ExceptionHandler({ NotFoundException.class })
+    public ResponseEntity<Body> handleNotFound(Exception e) {
+        log.error("Exception Caused By : {}", e.getMessage());
+        return Response.create(HttpStatus.NOT_FOUND, e.getMessage());
+    }
 
 }
