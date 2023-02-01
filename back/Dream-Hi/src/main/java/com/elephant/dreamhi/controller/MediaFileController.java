0cf773906698f6ed7bf3c46edf7bcc75f6fb65cb
JunHyxxn <ihrug000@gmail.com>
2023-02-01 16:15:32 +0900

    :sparkles: Feat: 미디어 파일 삭제 기능 구현 완료

--- back/Dream-Hi/src/main/java/com/elephant/dreamhi/controller/MediaFileController.java
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/controller/MediaFileController.java
@@ -1,13 +1,19 @@
 package com.elephant.dreamhi.controller;
 
 import com.elephant.dreamhi.model.dto.MediaFileDto;
+import com.elephant.dreamhi.security.PrincipalDetails;
 import com.elephant.dreamhi.service.MediaFileService;
 import com.elephant.dreamhi.utils.Response;
 import com.elephant.dreamhi.utils.Response.Body;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +35,26 @@ public class MediaFileController {
     public ResponseEntity<Body> getMediaFiles(@PathVariable Long id) {
         MediaFileDto responseDto = mediaFileService.findMediaFilesByActorProfileId(id);
         return Response.create(HttpStatus.OK, HttpStatus.OK.name(), responseDto);
+    }
+
+    /**
+     * MediaFile 삭제 메소드
+     *
+     * @param principalDetails        : 접근 중 주체
+     * @param actorProfileId          : 배우 프로필 id
+     * @param actorProfileMediaFileId : 미디어 파일 id
+     * @return 200
+     * @throws AccessDeniedException          : 사용자가 잘못된 배우 프로필 수정을 요청 시 권한 없음 발생
+     * @throws EmptyResultDataAccessException : 삭제할 데이터가 없는 경우 발생
+     */
+    @DeleteMapping("/api/actors/{actorProfileId}/media/{actorProfileMediaFileId}")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public ResponseEntity<Body> deleteMediaFile(@AuthenticationPrincipal PrincipalDetails principalDetails, @PathVariable Long actorProfileId,
+                                                @PathVariable Long actorProfileMediaFileId)
+            throws AccessDeniedException, EmptyResultDataAccessException {
+        log.info("id : {} {} {}", principalDetails.getId(), actorProfileId, actorProfileMediaFileId);
+        mediaFileService.deleteMediaFile(principalDetails.getId(), actorProfileId, actorProfileMediaFileId);
+        return Response.ok();
     }
 
 }
