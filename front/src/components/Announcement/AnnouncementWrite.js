9ac890624cb816b8bf9673b49696c7d23badd5bf
leeyuhmin <yuhmin92@naver.com>
2023-02-01 17:00:33 +0900

    :sparkles: Feat: 공고 등록  이미지 업로드 기능 추가

--- front/src/components/Announcement/AnnouncementWrite.js
+++ front/src/components/Announcement/AnnouncementWrite.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import AnnouncementWriteItem from './AnnouncementWriteItem';
+import Button from '../Common/Button';
+import { useNavigate } from 'react-router-dom';
+import AppHeader from '../Common/AppHeader';
 
 function AnnouncementWrite(props) {
+  const navigate = useNavigate();
   return (
     <div>
+      <AppHeader />
       <h1>공고 작성</h1>
+      <Button
+        title="뒤로 가기"
+        onClick={() => {
+          navigate("/announcement")
+        }}
+      />
       <AnnouncementWriteItem />
     </div>
   );
---
+++ front/src/components/Announcement/AnnouncementWriteImage.js
@@ -0,0 +1,38 @@
+import React, { useRef, useState } from 'react';
+import Button from '../Common/Button';
+
+function AnnouncementWriteImage() {
+  const [image, setImage] = useState('');
+  const imgRef = useRef();
+  const previewImage = () => {
+    const file = imgRef.current.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setImage(reader.result);
+    };
+  };
+  
+  function clickImageInput() {
+    const formData = new FormData()
+    formData.append('image', image)
+  }
+  return (
+    <div>
+      <h4>공고 사진 업로드</h4>
+      <img 
+      src={image ? image : `/logo192.png`}
+      alt = "공고 이미지"
+      />
+      <input type='file'
+             accept='image/jpg, image/png, image/jpeg, image/gif'
+             id='announcementImg' onChange={previewImage} ref={imgRef}/>
+      <Button
+      title = "이미지 변경"
+      onClick = {clickImageInput}
+      />
+    </div>
+  )
+}
+
+export default AnnouncementWriteImage;
