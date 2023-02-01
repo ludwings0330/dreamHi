b2b7e8f1f6270835e75430d3753bc7daf7e0ff29
JunHyxxn <ihrug000@gmail.com>
2023-02-01 12:32:17 +0900

    :sparkles: Feat: User의 메인 프로필 사진 변경 기능 구현 완료

--- back/Dream-Hi/src/main/java/com/elephant/dreamhi/controller/UserController.java
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/controller/UserController.java
@@ -1,16 +1,22 @@
 package com.elephant.dreamhi.controller;
 
+import com.elephant.dreamhi.model.dto.PictureDto;
 import com.elephant.dreamhi.model.dto.UserSimpleDto;
 import com.elephant.dreamhi.security.PrincipalDetails;
 import com.elephant.dreamhi.service.UserService;
 import com.elephant.dreamhi.utils.Response;
+import com.elephant.dreamhi.utils.Response.Body;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -33,6 +39,22 @@ public class UserController {
         PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
         UserSimpleDto userSimpleDto = userService.findUserSimple(principalDetails.getId());
         return Response.create(HttpStatus.OK, HttpStatus.OK.name(), userSimpleDto);
+    }
+
+    /**
+     * 메인 프로필 사진 변경 메소드
+     *
+     * @param id         : userId
+     * @param pictureDto : PictureDTO
+     * @return 200
+     * @throws UsernameNotFoundException : userId에 해당하는 유저가 존재하지않은 경우 발생합니다.
+     */
+    @PostMapping("/api/users/{id}/main-profile")
+    @PreAuthorize("hasRole('ROLE_USER') and principal.id == #id")
+    public ResponseEntity<Body> changeMainProfile(@PathVariable Long id, @RequestBody PictureDto pictureDto) throws UsernameNotFoundException {
+        log.info("id : {} picture : {}"  , id, pictureDto);
+        userService.updateMainProfile(id, pictureDto);
+        return Response.ok();
     }
 
 }
--- back/Dream-Hi/src/main/java/com/elephant/dreamhi/service/UserService.java
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/service/UserService.java
@@ -1,10 +1,13 @@
 package com.elephant.dreamhi.service;
 
+import com.elephant.dreamhi.model.dto.PictureDto;
 import com.elephant.dreamhi.model.dto.UserSimpleDto;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public interface UserService {
 
     UserSimpleDto findUserSimple(Long id) throws UsernameNotFoundException;
+
+    void updateMainProfile(Long id, PictureDto pictureDto) throws UsernameNotFoundException;
 
 }
