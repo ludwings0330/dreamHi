7fbf1f8da8c4b094a116e2dae594be993ac053ec
JunHyxxn <ihrug000@gmail.com>
2023-02-02 10:16:14 +0900

    :sparkles: Feat: FullResourceException 추가
    허용된 Resource 사용량 초과 시 발생하는 예외 추가

---
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/exception/FullResourceException.java
@@ -0,0 +1,13 @@
+package com.elephant.dreamhi.exception;
+
+public class FullResourceException extends RuntimeException{
+
+    public FullResourceException() {
+        super();
+    }
+
+    public FullResourceException(String message) {
+        super(message);
+    }
+
+}
