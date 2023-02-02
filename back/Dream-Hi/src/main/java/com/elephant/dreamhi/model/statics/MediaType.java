f8a77c1940cfab6af11fea0866bf77e765de29a0
JunHyxxn <ihrug000@gmail.com>
2023-02-02 10:14:26 +0900

    :sparkles: Feat: EnumType Converter 구현
    DTO에 Enum Type 대문자만 인식이 됐는데 소문자로도 변환되도록 @JsonCreator 추가

--- back/Dream-Hi/src/main/java/com/elephant/dreamhi/model/statics/MediaType.java
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/model/statics/MediaType.java
@@ -1,6 +1,14 @@
 package com.elephant.dreamhi.model.statics;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum MediaType {
     PICTURE,
-    VIDEO
+    VIDEO;
+
+    @JsonCreator
+    public static MediaType from(String s) {
+        return MediaType.valueOf(s.toUpperCase());
+    }
+
 }
--- back/Dream-Hi/src/main/java/com/elephant/dreamhi/model/statics/UserRole.java
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/model/statics/UserRole.java
@@ -1,8 +1,15 @@
 package com.elephant.dreamhi.model.statics;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum UserRole {
 
     ROLE_USER,
-    ROLE_ANONYMOUS
+    ROLE_ANONYMOUS;
+
+    @JsonCreator
+    public static UserRole from(String s) {
+        return UserRole.valueOf(s.toUpperCase());
+    }
 
 }
