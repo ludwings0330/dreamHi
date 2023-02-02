30d897664d6fd7e88327d6fad43eea40fbd2cc6e
JunHyxxn <ihrug000@gmail.com>
2023-02-02 10:14:26 +0900

    :sparkles: Feat: EnumType Converter 구현
    DTO에 Enum Type 대문자만 인식이 됐는데 소문자로도 변환되도록 @JsonCreator 추가

--- back/Dream-Hi/src/main/java/com/elephant/dreamhi/model/statics/VolunteerState.java
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/model/statics/VolunteerState.java
@@ -1,10 +1,17 @@
 package com.elephant.dreamhi.model.statics;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum VolunteerState {
 
     NONE,
     PASS,
     FAIL,
-    WAIT
+    WAIT;
+
+    @JsonCreator
+    public static VolunteerState from(String s) {
+        return VolunteerState.valueOf(s.toUpperCase());
+    }
 
 }
