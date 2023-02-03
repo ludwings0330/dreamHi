edb256eb0c9da0c34da0764fab171e558bb655d5
JunHyxxn <ihrug000@gmail.com>
2023-02-03 13:49:16 +0900

    :sparkles: Feat: 스타일 태그 삭제, 조회를 위한 Repository 구현

--- back/Dream-Hi/src/main/java/com/elephant/dreamhi/repository/ActorStyleRelationRepository.java
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/repository/ActorStyleRelationRepository.java
@@ -1,10 +1,21 @@
 package com.elephant.dreamhi.repository;
 
 import com.elephant.dreamhi.model.entity.ActorStyleRelation;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ActorStyleRelationRepository extends JpaRepository<ActorStyleRelation, Long> {
+public interface ActorStyleRelationRepository extends JpaRepository<ActorStyleRelation, Long>, ActorStyleRelationRepositoryCustom {
+
+    @Query("DELETE "
+            + "FROM ActorStyleRelation asr "
+            + "WHERE asr.actorProfile.id = :actorProfileId "
+            + "AND asr.style.id in :deleteStyles")
+    @Modifying
+    Integer deleteAllInStlyeIdQuery(@Param("actorProfileId") Long actorProfileId, @Param("deleteStyles") List<Long> deleteStyles);
 
 }
--- back/Dream-Hi/src/main/java/com/elephant/dreamhi/repository/StyleRepository.java
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/repository/StyleRepository.java
@@ -4,7 +4,6 @@ import com.elephant.dreamhi.model.entity.Style;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface StyleRepository extends JpaRepository<Style, Long> {
 
 }
