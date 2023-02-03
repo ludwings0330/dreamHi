2b470ac5336612aafef1901f2526c742918a29fc
hyosang <ddf998@naver.com>
2023-02-03 09:32:40 +0900

    :sparkles: Feat:  화상면접 예ì날짜 선택 페이지 추가

--- front/src/pages/AuditionPage.js
+++ front/src/pages/AuditionPage.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import Layout from '../components/Common/Layout';
 import Button from '../components/Common/Button';
-import { useNavigate } from 'react-router-dom';
+import CalendarAnnounce from '../components/Calendar/calendarAnnounce';
+
+import Notice from '../components/Audition/Notice';
+import styled, { css } from 'styled-components'
 
 function AuditionPage(props) {
   const navigate = useNavigate()
@@ -13,9 +18,33 @@ function AuditionPage(props) {
           navigate("/audition/meeting")
         }}
       />
+      <AuditionContent>
+        <AuditionContentLeft>
+          <CalendarAnnounce />
+        </AuditionContentLeft>
+        <AuditionContentRight>
+          <Notice />
+        </AuditionContentRight>
+      </AuditionContent>
 
     </Layout>
   );
 }
 
 export default AuditionPage;
+
+const AuditionContent = styled.div`
+  display: flex;
+  left: 50%;
+  position: absolute;
+  transform: translateX(-50%);
+  margin-top: 30px;
+  `
+
+const AuditionContentLeft = styled.div`
+  margin-right: 200px;
+`
+
+const AuditionContentRight = styled.div`
+
+`
