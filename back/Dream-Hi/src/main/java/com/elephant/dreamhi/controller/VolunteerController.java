0d8ad05c5f1c447f869a4dadfccd560d88732bec
ludwings0330 <ludwings0330@gmail.com>
2023-02-03 17:57:11 +0900

    :sparkles: Feat: 지원자 관리 기능 생성

---
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/controller/VolunteerController.java
@@ -0,0 +1,33 @@
+package com.elephant.dreamhi.controller;
+
+import com.elephant.dreamhi.model.dto.VolunteerApplyRequestDto;
+import com.elephant.dreamhi.security.PrincipalDetails;
+import com.elephant.dreamhi.service.VolunteerService;
+import com.elephant.dreamhi.utils.Response;
+import com.elephant.dreamhi.utils.Response.Body;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class VolunteerController {
+
+    private final VolunteerService volunteerService;
+
+    @PostMapping("/api/announcements/{announcementId}/volunteers")
+    public ResponseEntity<Body> apply(@PathVariable Long announcementId,
+                                      @RequestBody VolunteerApplyRequestDto requestDto,
+                                      @AuthenticationPrincipal PrincipalDetails user) {
+        volunteerService.userApplyOnAnnouncement(user.getId(), requestDto);
+        return Response.create(HttpStatus.CREATED, "지원이 완료되었습니다");
+    }
+
+}
