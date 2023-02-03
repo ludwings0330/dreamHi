91443e0011f01045d876dcaef0077d8caaf6f7b2
JunHyxxn <ihrug000@gmail.com>
2023-02-03 13:49:16 +0900

    :sparkles: Feat: 스타일 태그 삭제, 조회를 위한 Repository 구현

---
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/repository/StyleRepository.java
@@ -0,0 +1,8 @@
+package com.elephant.dreamhi.repository;
+
+import com.elephant.dreamhi.model.entity.Style;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StyleRepository extends JpaRepository<Style, Long> {
+
+}
