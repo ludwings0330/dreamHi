f83a2e1d8a02817039b9c9d522b942ef3c1e26d9
이다운 <innocent61@naver.com>
2023-02-03 18:01:11 +0900

    :sparkles: Feat: 공고 저장 구현 중

---
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/model/dto/AnnouncementSaveDto.java
@@ -0,0 +1,42 @@
+package com.elephant.dreamhi.model.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class AnnouncementSaveDto {
+
+    @NotNull
+    @Size(max = 20)
+    private String title;
+
+    @NotNull
+    private ProducerAnnouncementDto producer;
+
+    @Size(max = 25)
+    private String payment;
+
+    @Size(max = 30)
+    private String crankPeriod;
+
+    @NotNull
+    private LocalDateTime endDate;
+
+    private String description;
+
+    private String pictureUrl;
+
+    @NotEmpty
+    private List<CastingSaveDto> castings;
+
+}
