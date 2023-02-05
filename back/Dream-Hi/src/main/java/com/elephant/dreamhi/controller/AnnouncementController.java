2fd2dd08a2d69423b40261cce61205ad2289a5f1
DaWoon Lee <qkgk61@gmail.com>
2023-02-05 15:31:10 +0900

    :sparkles: Feat: 공고를 저장하는 API 구현 완료
    공고 저장은 Authority가 Editor여야 한다.

--- back/Dream-Hi/src/main/java/com/elephant/dreamhi/controller/AnnouncementController.java
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/controller/AnnouncementController.java
@@ -2,6 +2,7 @@ package com.elephant.dreamhi.controller;
 
 import com.elephant.dreamhi.exception.NotFoundException;
 import com.elephant.dreamhi.model.dto.AnnouncementDetailDto;
+import com.elephant.dreamhi.model.dto.AnnouncementSaveDto;
 import com.elephant.dreamhi.model.dto.AnnouncementSearchCondition;
 import com.elephant.dreamhi.model.dto.AnnouncementSimpleDto;
 import com.elephant.dreamhi.model.dto.CastingDetailDto;
@@ -13,6 +14,7 @@ import com.elephant.dreamhi.service.ProcessService;
 import com.elephant.dreamhi.utils.Response;
 import com.elephant.dreamhi.utils.Response.Body;
 import java.util.List;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -26,6 +28,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -99,9 +102,20 @@ public class AnnouncementController {
         return Response.create(HttpStatus.OK, "OK", userStageDto);
     }
 
+    /**
+     *
+     * @param announcementSaveDto 공고를 저장할 때 필요한 데이터를 Request Body로 입력받는다.
+     * @param user                현재 로그인한 유저
+     * @return 저장에 성공한 경우 201, CREATED 반환
+     * @throws NotFoundException 저장하려는 배역의 스타일이 DB에 존재하지 않는 경우 발생하는 예외
+     */
     @PostMapping
-    @PreAuthorize("@checker.hasEditorAuthority(#user, #producer.id)")
-    public ResponseEntity<Body> saveAnnouncementDetail(, @AuthenticationPrincipal PrincipalDetails user) {
+    @PreAuthorize("@checker.hasEditorAuthority(#user, #announcementSaveDto.producer.id)")
+    public ResponseEntity<Body> saveAnnouncement(
+            @RequestBody @Valid AnnouncementSaveDto announcementSaveDto,
+            @AuthenticationPrincipal PrincipalDetails user
+    ) throws NotFoundException {
+        announcementService.saveAnnouncementDetail(announcementSaveDto);
         return Response.create(HttpStatus.CREATED, "CREATED");
     }
 
