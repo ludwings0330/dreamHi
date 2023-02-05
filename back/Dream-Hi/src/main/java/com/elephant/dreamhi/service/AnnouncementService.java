bcc13f4e5060479c08defe6ca3da0a90478f580d
DaWoon Lee <qkgk61@gmail.com>
2023-02-05 15:26:23 +0900

    :sparkles: Feat: 공고를 저장하는 메서드 구현
    공고 저장할 때 새로운 프로세스를 모집 중 상태로 함께 저장한다.

--- back/Dream-Hi/src/main/java/com/elephant/dreamhi/service/AnnouncementService.java
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/service/AnnouncementService.java
@@ -4,6 +4,7 @@ import com.elephant.dreamhi.exception.NotFoundException;
 import com.elephant.dreamhi.model.dto.AnnouncementDetailDto;
 import com.elephant.dreamhi.model.dto.AnnouncementSearchCondition;
 import com.elephant.dreamhi.model.dto.AnnouncementSimpleDto;
+import com.elephant.dreamhi.model.dto.AnnouncementSaveDto;
 import com.elephant.dreamhi.security.PrincipalDetails;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,5 +14,7 @@ public interface AnnouncementService {
     AnnouncementDetailDto findDetail(Long id, PrincipalDetails user) throws NotFoundException;
 
     Page<AnnouncementSimpleDto> findList(AnnouncementSearchCondition searchCondition, Pageable pageable, PrincipalDetails user);
+
+    void saveAnnouncementDetail(AnnouncementSaveDto announcementSaveDto) throws NotFoundException;
 
 }
--- back/Dream-Hi/src/main/java/com/elephant/dreamhi/service/AnnouncementServiceImpl.java
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/service/AnnouncementServiceImpl.java
@@ -2,23 +2,44 @@ package com.elephant.dreamhi.service;
 
 import com.elephant.dreamhi.exception.NotFoundException;
 import com.elephant.dreamhi.model.dto.AnnouncementDetailDto;
+import com.elephant.dreamhi.model.dto.AnnouncementSaveDto;
+import com.elephant.dreamhi.model.entity.Announcement;
+import com.elephant.dreamhi.model.entity.Casting;
+import com.elephant.dreamhi.model.entity.CastingStyleRelation;
+import com.elephant.dreamhi.model.entity.Process;
+import com.elephant.dreamhi.model.entity.Producer;
+import com.elephant.dreamhi.model.entity.Style;
 import com.elephant.dreamhi.model.dto.AnnouncementSearchCondition;
 import com.elephant.dreamhi.model.dto.AnnouncementSimpleDto;
 import com.elephant.dreamhi.model.dto.ProcessStageDto;
 import com.elephant.dreamhi.repository.AnnouncementRepository;
+import com.elephant.dreamhi.repository.CastingRepository;
+import com.elephant.dreamhi.repository.CastingStyleRelationRepository;
+import com.elephant.dreamhi.repository.ProcessRepository;
+import com.elephant.dreamhi.repository.ProducerRepository;
+import com.elephant.dreamhi.repository.StyleRepository;
 import com.elephant.dreamhi.security.PrincipalDetails;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class AnnouncementServiceImpl implements AnnouncementService {
 
+    private final ProcessRepository processRepository;
+
+    private final ProducerRepository producerRepository;
     private final AnnouncementRepository announcementRepository;
+    private final CastingRepository castingRepository;
+    private final StyleRepository styleRepository;
+    private final CastingStyleRelationRepository castingStyleRelationRepository;
     private final ProcessService processService;
 
     /**
@@ -51,6 +72,30 @@ public class AnnouncementServiceImpl implements AnnouncementService {
     public AnnouncementDetailDto findDetail(Long announcementId, PrincipalDetails user) throws NotFoundException {
         return announcementRepository.findByAnnouncementIdAndFollowerId(announcementId, user.getId())
                                      .orElseThrow(() -> new NotFoundException("해당 공고를 찾을 수 없습니다."));
+    }
+
+    @Override
+    @Transactional
+    public void saveAnnouncementDetail(AnnouncementSaveDto announcementSaveDto) {
+        Producer producer = producerRepository.getReferenceById(announcementSaveDto.getProducer().getId());
+        Announcement announcement = Announcement.toEntity(announcementSaveDto, producer);
+        announcementRepository.save(announcement);
+        processRepository.save(Process.getInstanceForRecruiting(announcement));
+
+        announcementSaveDto.getCastings().forEach(castingSaveDto -> {
+            Casting casting = Casting.toEntity(announcement, castingSaveDto);
+            castingRepository.save(casting);
+
+            List<CastingStyleRelation> castingStyleRelations = new ArrayList<>();
+            castingSaveDto.getStyles().forEach(styleDto -> {
+                Style style = styleRepository.findById(styleDto.getId())
+                                             .orElseThrow(() -> new NotFoundException("해당 스타일을 찾을 수 없습니다."));
+                style.addCount();
+                castingStyleRelations.add(CastingStyleRelation.toEntity(casting, style));
+            });
+
+            castingStyleRelationRepository.saveAll(castingStyleRelations);
+        });
     }
 
 }
