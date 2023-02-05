0dbda033f199bbe8a97f6dfc55d12548cc8c1b49
leeymk <leeymk11@gmail.com>
2023-02-06 07:26:24 +0900

    :sparkles: 가장 최근 업로드 이미지만 표시

--- front/src/imageup/firebase.js
+++ front/src/imageup/firebase.js
@@ -4,6 +4,18 @@ const firebaseConfig = {
 
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
+
+
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from 'firebase/firestore';
+//
+//
+// const firebaseConfig = {
+//
+// };
+//
+// const app = initializeApp(firebaseConfig);
+//
+// export const db = getFirestore(app);
