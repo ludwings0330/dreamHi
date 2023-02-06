b8fac29fc8e4766bc62d895d643a0c5bae7e7480
이다운 <innocent61@naver.com>
2023-02-06 14:45:38 +0900

    :hammer: Refactor: 사용하지 않는 메서드 삭제

--- back/Dream-Hi/src/main/java/com/elephant/dreamhi/repository/AnnouncementRepositoryCustom.java
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/repository/AnnouncementRepositoryCustom.java
@@ -3,14 +3,11 @@ package com.elephant.dreamhi.repository;
 import com.elephant.dreamhi.model.dto.AnnouncementDetailDto;
 import com.elephant.dreamhi.model.dto.AnnouncementSearchCondition;
 import com.elephant.dreamhi.model.dto.AnnouncementSimpleDto;
-import com.elephant.dreamhi.model.entity.Announcement;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface AnnouncementRepositoryCustom {
-
-    Optional<Announcement> findAnnouncementById(Long id);
 
     Optional<AnnouncementDetailDto> findByAnnouncementIdAndFollowerId(Long announcementId, Long followerId);
 
