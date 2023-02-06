5e45b88c75770e824917d9a7508d120713b639f5
DaWoon Lee <qkgk61@gmail.com>
2023-02-06 22:38:31 +0900

    :sparkles: Feat: ProcessController에서 Process를 저장하는 메서드 구현

--- back/Dream-Hi/src/main/java/com/elephant/dreamhi/controller/ProcessController.java
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/controller/ProcessController.java
@@ -1,12 +1,17 @@
 package com.elephant.dreamhi.controller;
 
+import com.elephant.dreamhi.model.dto.ProcessSaveDto;
 import com.elephant.dreamhi.security.PrincipalDetails;
+import com.elephant.dreamhi.service.ProcessService;
+import com.elephant.dreamhi.utils.Response;
 import com.elephant.dreamhi.utils.Response.Body;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ProcessController {
 
+    private final ProcessService processService;
+
     @PostMapping
-    @PreAuthorize("@checker.hasEditorAuthority(#user, #producerId)")
-    public ResponseEntity<Body> updateProcess(@AuthenticationPrincipal PrincipalDetails user) {
-        return null;
+    @PreAuthorize("@checker.hasEditorAuthority(#user, #processSaveDto.producerId)")
+    public ResponseEntity<Body> saveProcess(@RequestBody @Valid ProcessSaveDto processSaveDto, @AuthenticationPrincipal PrincipalDetails user) {
+        processService.saveProcess(processSaveDto);
+        return Response.created();
     }
 
 }
