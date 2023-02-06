9ad4d5420b61f924c014d5754f5e99a3bcb35e89
aldls819 <aldls819@gmail.com>
2023-02-06 13:03:56 +0900

    :sparkles: Feat: Maker Component 완성 및 CSS 수정 필요 Open-vidu 초기설정

--- front/src/components/Actor/video/ActorVideo.js
+++ front/src/components/Actor/video/ActorVideo.js
@@ -5,24 +5,27 @@ const products = [
   {
     id: 1,
     name: 'Basic Tee',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+    imageSrc: 'https://cdn.pixabay.com/vimeo/780232342/%EC%86%8C%EB%82%98%EB%AC%B4%20-%20142579.mp4?width=640&hash=cc422ffdf710dae6979b15e61522023b232e3d4d',
   },
   {
     id: 2,
     name: 'Basic Tee',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+    imageSrc: 'https://cdn.pixabay.com/vimeo/629483574/%EC%98%A4%EB%A1%9C%EB%9D%BC%20%EB%B3%B4%20%EB%A6%AC%20%EC%96%BC%20%EB%A6%AC%EC%8A%A4%20-%2090877.mp4?width=960&hash=ed2df50dc5d55a8fbcad914dab6a4e6214fe2845',
   },
   {
     id: 3,
     name: 'Basic Tee',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+    imageSrc: 'https://cdn.pixabay.com/vimeo/764361528/%EB%8C%80%EC%96%91%20-%20135658.mp4?width=1280&hash=29d5a82cfe208bfbe486b1faa182cd11fce9efa1',
   },
   {
     id: 4,
     name: 'Basic Tee',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+    imageSrc: 'https://cdn.pixabay.com/vimeo/694704491/%EB%82%98%EB%AC%B4%20%EB%A7%90%EB%AF%B8%EC%9E%98%20-%20112429.mp4?width=640&hash=cc4b82f4fba0f71bc19b80f1df7a3470bd89f181',
   },
 ]
+const setSelected = (idx) => {
+  document.querySelector('.video-main').innerHTML=`<video src=${products[idx].imageSrc} alt=${products[idx].imageAlt} controls/>`
+};
 
 
 const ActorVideo = () => {
@@ -33,20 +36,21 @@ const ActorVideo = () => {
         <h1>연기영상</h1>
 
         <div className="list-container">
-          <div className='photo-main'>
-            <img src={products[0].imageSrc} alt={products[0].imageAlt}/>
+          <div className='video-main'>
+            <video  src={products[0].imageSrc} alt={products[0].imageAlt} controls/>
           </div>
 
-          <div className='photo-list'>
+          <div className='video-list'>
             {products.map((product, idx) => (
               idx === 0 ? null :
-                <div className='photo' key={product.id}>
-                  <img
+                <div className='video' key={product.id}>
+                  <video
                     src={product.imageSrc}
                     alt={product.imageAlt}
                     width={"200px"}
                     height={"200px"}
                     className="object-center"
+                    onClick={() => setSelected(idx)}
                   />
                 </div>
             ))}
