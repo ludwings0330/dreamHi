c9c01daf891a9576694141f0beb0594615c07afd
이다운 <innocent61@naver.com>
2023-02-07 14:08:17 +0900

    :hammer: Refactor: DTO에 toString 메서드 추가

--- back/Dream-Hi/src/main/java/com/elephant/dreamhi/model/dto/AnnouncementSimpleDto.java
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/model/dto/AnnouncementSimpleDto.java
@@ -6,11 +6,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Builder
+@ToString
 public class AnnouncementSimpleDto {
 
     private Long id;
--- back/Dream-Hi/src/main/java/com/elephant/dreamhi/model/dto/ProcessSaveDto.java
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/model/dto/ProcessSaveDto.java
@@ -7,11 +7,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Builder
+@ToString
 public class ProcessSaveDto {
 
     @NotNull
