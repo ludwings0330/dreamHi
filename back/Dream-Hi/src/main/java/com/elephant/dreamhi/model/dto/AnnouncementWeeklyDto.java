83bf3abf236c6ad60d393e6f9c6850c47547567c
이다운 <innocent61@naver.com>
2023-02-07 16:35:57 +0900

    :sparkles: Feat: 주간 공고 응답에 사용할 DTO 정의

---
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/model/dto/AnnouncementWeeklyDto.java
@@ -0,0 +1,26 @@
+package com.elephant.dreamhi.model.dto;
+
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+@ToString
+public class AnnouncementWeeklyDto {
+
+    @NotNull
+    private Long id;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String producerName;
+
+}
