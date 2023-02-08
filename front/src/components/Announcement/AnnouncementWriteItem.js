34928e19f3679ba6658394596f855584f8b84b2b
leeymk <leeymk11@gmail.com>
2023-02-09 06:38:46 +0900

    :sparkles: Feat: 공고 등록 데이터 작성

--- front/src/components/Announcement/AnnouncementWriteItem.js
+++ front/src/components/Announcement/AnnouncementWriteItem.js
@@ -1,85 +1,36 @@
 import React from 'react';
-import ImageUpload from '../../imageup/ImageUpload';
-
-import { useForm, Controller } from 'react-hook-form';
-import ReactDatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
-import AnnouncementWriteImage from './AnnouncementWriteImage';
-
-const defaultValues = {
-  ReactDatepicker: new Date(),
-};
-
-const Input = ({ label, register, required }) => (
-  <>
-    <label>{label}</label>
-    <input {...register(label, { required })} />
-  </>
-);
-
-const Select = React.forwardRef(({ onChange, name, label }, ref) => (
-  <>
-    <label>{label}</label>
-    <select name={name} ref={ref} onChange={onChange}>
-      <option value="baby">~</option>
-      <option value="10">10</option>
-      <option value="15">15</option>
-      <option value="10">20</option>
-      <option value="10">25</option>
-      <option value="10">30</option>
-      <option value="10">35</option>
-      <option value="10">40</option>
-      <option value="10">45</option>
-      <option value="10">50</option>
-      <option value="10">60</option>
-      <option value="10">70</option>
-    </select>
-  </>
-));
+import {useRecoilValue} from "../../recoil";
+import {
+    announcementCrankPeriod, announcementDescription, announcementEndDate,
+    announcementPayment,
+    announcementProducerId,
+    announcementTitle
+} from "../../recoil/announcement";
+import {useRecoilState} from "recoil";
 
 function AnnouncementWriteItem(props) {
-  const { register, handleSubmit } = useForm();
 
-  const onSubmit = (data) => {
-    alert(JSON.stringify(data));
-  };
-  const { control } = useForm({ defaultValues });
 
-  return (
-    <div>
-      <h2>공고 리스트 작성 페이지</h2>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Input label="작품명" register={register} required />
-        <Input label="제작사" register={register} required />
-        <Input label="출연료" register={register} required />
-        <Input label="촬영 기간" register={register} required />
-        <Select label="나이" {...register('Age')} />
-        <span>~</span>
-        <Select {...register('Age')} />
-        <br></br>
-        <Select label="키" {...register('Age')} />
-        <span>~</span>
-        <Select {...register('Age')} />
-        <section>
-          <label>공고 마감일</label>
-          <Controller
-            control={control}
-            name="ReactDatepicker"
-            render={({ field }) => (
-              <ReactDatePicker
-                className="input"
-                placeholderText="Select date"
-                onChange={(e) => field.onChange(e)}
-                selected={field.value}
-              />
-            )}
-          />
-        </section>
-      </form>
-      <AnnouncementWriteImage />
-    </div>
-  );
+    const [dataTitle, setDataTitle] = useRecoilState(announcementTitle);
+    const [dataProducerId, setDataProducerId] = useRecoilState(announcementProducerId);
+    const [dataPayment, setDataPayment] = useRecoilState(announcementPayment);
+    const [dataCrankPeriod, setDataCrankPeriod] = useRecoilState(announcementCrankPeriod);
+    const [dataEndDate, setDataEndDate] = useRecoilState(announcementEndDate);
+    const [dataDescription, SetDataDescription] = useRecoilState(announcementDescription);
+
+    const handleDataChange = (e) => {
+        setDataTitle({ ...dataTitle, [e.target.name]: e.target.value });
+    };
+
+
+
+    return (
+        <div>
+
+
+        </div>
+    );
 }
 
 export default AnnouncementWriteItem;
