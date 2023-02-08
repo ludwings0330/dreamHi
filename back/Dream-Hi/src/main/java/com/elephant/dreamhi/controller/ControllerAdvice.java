590d24fab1ba30afb453d585946b025c51a56c51
JunHyxxn <ihrug000@gmail.com>
2023-02-09 00:36:03 +0900

    :hammer: Fix: Advice Exception 처리 추가

--- back/Dream-Hi/src/main/java/com/elephant/dreamhi/controller/ControllerAdvice.java
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/controller/ControllerAdvice.java
@@ -1,10 +1,14 @@
 package com.elephant.dreamhi.controller;
 
+import com.elephant.dreamhi.exception.FullResourceException;
 import com.elephant.dreamhi.exception.NotFoundException;
+import com.elephant.dreamhi.exception.VisibleException;
 import com.elephant.dreamhi.utils.Response;
 import com.elephant.dreamhi.utils.Response.Body;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
@@ -16,7 +20,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ControllerAdvice {
 
-    @ExceptionHandler({ DuplicateKeyException.class })
+    @ExceptionHandler({ DuplicateKeyException.class, VisibleException.class, DataIntegrityViolationException.class, EmptyResultDataAccessException.class })
     public ResponseEntity<Body> handleConflict(Exception e) {
         return Response.create(HttpStatus.CONFLICT, e.getMessage());
     }
@@ -26,7 +30,7 @@ public class ControllerAdvice {
         return Response.create(HttpStatus.NOT_FOUND, e.getMessage());
     }
 
-    @ExceptionHandler({ IllegalArgumentException.class })
+    @ExceptionHandler({ FullResourceException.class, IllegalArgumentException.class })
     public ResponseEntity<Body> handleBadRequest(Exception e) {
         return Response.create(HttpStatus.BAD_REQUEST, e.getMessage());
     }
