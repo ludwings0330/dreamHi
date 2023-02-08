e03a1555bba5d45868a9b651f4a59f70ac942fcb
hyosang <ddf998@naver.com>
2023-02-08 17:41:26 +0900

    :poop: Fix: merge request 오류

--- front/src/components/Actor/photo/ActorPhotoUpload.js
+++ front/src/components/Actor/photo/ActorPhotoUpload.js
@@ -14,6 +14,7 @@ import '../../../components/Casting/Casting.css';
 import './ActorPhoto.css';
 
 function ActorPhotoUpload(props) {
+  console.log('test')
   const [ActorPhotoUploaded, setActorPhotoUploaded] = useState(null);
   // const [actPhotoUrl, setActPhotoUrl] = useState('');
   // const [actorPhotos, setActorPhotos] = useRecoilState(actorPhotoLists);
