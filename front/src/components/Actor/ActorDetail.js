7611f64e50254a5edf921b9a66da1f0ff0f27aad
aldls819 <aldls819@gmail.com>
2023-02-08 17:08:38 +0900

    :sparkles: Feat: Maker List Axios 설정 중

--- front/src/components/Actor/ActorDetail.js
+++ front/src/components/Actor/ActorDetail.js
@@ -2,7 +2,6 @@ import React, {useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
 
-import Layout from '../Common/MainLayout/Layout';
 import ActorIntroduce from './info/ActorIntroduce';
 import ActorFilmo from './filmo/ActorFilmo';
 import ActorPhoto from './photo/ActorPhoto';
