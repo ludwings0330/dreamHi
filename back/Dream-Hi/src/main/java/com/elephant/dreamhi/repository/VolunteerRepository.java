d7fc283a5adedfe659454cc25099db8682e28cec
이다운 <innocent61@naver.com>
2023-02-14 01:21:58 +0900

    :hammer: Refactor: UserId와 ProcessId로 지원자 목록를 조회하는 기능은 쿼리 메서드로 수행 가능하므로 기존 구현 삭제

--- back/Dream-Hi/src/main/java/com/elephant/dreamhi/repository/VolunteerRepository.java
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/repository/VolunteerRepository.java
@@ -1,10 +1,13 @@
 package com.elephant.dreamhi.repository;
 
 import com.elephant.dreamhi.model.entity.Volunteer;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VolunteerRepository extends JpaRepository<Volunteer, Long>, VolunteerRepositoryCustom {
+
+    List<Volunteer> findByUserIdAndProcessId(Long userId, Long processId);
 
 }
