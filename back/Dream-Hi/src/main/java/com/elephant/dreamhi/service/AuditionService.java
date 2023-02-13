0014dbe18b57cb46ad5e3cbb42ce49834526065b
이다운 <innocent61@naver.com>
2023-02-13 21:50:50 +0900

    :sparkles: Feat: AuditionServiceImpl에서 예약 일자에 대한 예약 현황을 조회하는 기능 구현

--- back/Dream-Hi/src/main/java/com/elephant/dreamhi/service/AuditionService.java
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/service/AuditionService.java
@@ -2,15 +2,23 @@ package com.elephant.dreamhi.service;
 
 import com.elephant.dreamhi.exception.NotFoundException;
 import com.elephant.dreamhi.model.dto.BookPeriod;
+import com.elephant.dreamhi.model.dto.BookProducerDto;
+import com.elephant.dreamhi.model.dto.BookResponseDto;
+import java.time.LocalDate;
+import java.util.List;
 
 public interface AuditionService {
+
+    BookPeriod findBookPeriod(Long processId);
+
+    List<BookResponseDto> findAllBookForVolunteer(Long processId, LocalDate date);
+
+    List<BookProducerDto> findAllBookForProducer(Long producerId, LocalDate date);
 
     String findFileUrl(Long processId) throws NotFoundException;
 
     String findSessionId(Long processId) throws NotFoundException;
 
     void saveSession(Long processId, String fileUrl) throws NotFoundException, IllegalArgumentException;
-
-    BookPeriod findBookPeriod(Long processId);
 
 }
