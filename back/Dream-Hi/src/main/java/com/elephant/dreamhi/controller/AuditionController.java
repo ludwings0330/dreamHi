c5ba41d652247edf39ba70bb6c3b1e3088fa2e69
이다운 <innocent61@naver.com>
2023-02-14 01:33:49 +0900

    :sparkles: Feat: AuditionController에서 지원자의 화상 오디션 일정 예약 기능 구현

--- back/Dream-Hi/src/main/java/com/elephant/dreamhi/controller/AuditionController.java
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/controller/AuditionController.java
@@ -1,7 +1,8 @@
 package com.elephant.dreamhi.controller;
 
 import com.elephant.dreamhi.exception.NotFoundException;
-import com.elephant.dreamhi.model.dto.BookPeriod;
+import com.elephant.dreamhi.model.dto.BookPeriodDto;
+import com.elephant.dreamhi.model.dto.BookRequestDto;
 import com.elephant.dreamhi.model.dto.BookResponseDto;
 import com.elephant.dreamhi.security.PrincipalDetails;
 import com.elephant.dreamhi.service.AuditionService;
@@ -51,7 +52,7 @@ public class AuditionController {
     /**
      * @param processId      오디션의 현재 절차 ID
      * @param announcementId 현재 공고 ID
-     * @param producerId     현재 공고를 작성한 제작사 ID
+     * @param producerId     현재 공고를 작성한 제작사 ID, 현재 유저가 지원자인 경우 null을 받아야 한다.
      * @param user           현재 로그인 한 유저
      * @return 예약 가능한 날짜의 시작일과 종료일을 Response의 Body에 담아서 반환
      * @throws NotFoundException 제작사에서 화상 오디션 예약 가능한 일자를 등록하지 않은 경우에 발생하는 예외
@@ -64,14 +65,14 @@ public class AuditionController {
             @RequestParam(name = "pid", required = false) Long producerId,
             @AuthenticationPrincipal PrincipalDetails user
     ) throws NotFoundException {
-        BookPeriod bookPeriod = auditionService.findBookPeriod(processId);
-        return Response.create(HttpStatus.OK, "예약 가능한 날짜의 시작일과 종료일을 조회했습니다.", bookPeriod);
+        BookPeriodDto bookPeriodDto = auditionService.findBookPeriod(processId);
+        return Response.create(HttpStatus.OK, "예약 가능한 날짜의 시작일과 종료일을 조회했습니다.", bookPeriodDto);
     }
 
     /**
      * @param processId      오디션의 현재 절차 ID
      * @param announcementId 현재 공고 ID
-     * @param producerId     현재 공고를 작성한 제작사 ID
+     * @param producerId     현재 공고를 작성한 제작사 ID, 현재 유저가 지원자인 경우 null을 받아야 한다.
      * @param date           선택한 예약 날짜
      * @param user           현재 로그인 한 유저
      * @return 지원자에게는 예약 현황을, 제작사에게는 각 예약 현황에 지원한 User의 ID를 Response의 Body에 담아서 반환
@@ -99,7 +100,7 @@ public class AuditionController {
     /**
      * @param processId      오디션의 현재 절차 ID
      * @param announcementId 현재 공고 ID
-     * @param producerId     현재 공고를 작성한 제작사 ID
+     * @param producerId     현재 공고를 작성한 제작사 ID, 현재 유저가 지원자인 경우 null을 받아야 한다.
      * @param user           현재 로그인 한 유저
      * @return 화상 오디션을 위한 파일 URL 조회 결과를 Response의 Body에 담아서 반환
      * @throws NotFoundException 현재 절차 ID에 대하여 사전에 저장해 둔 세션 정보가 없을 때 발생하는 예외
@@ -137,9 +138,11 @@ public class AuditionController {
     }
 
     /**
-     * @param processId 오디션의 현재 절차 ID, stage가 화상 오디션이어야 한다.
-     * @param fileUrl   화상 오디션을 위한 참고 파일을 저장해 둔 URL
-     * @param user      현재 로그인 한 유저
+     * @param processId      오디션의 현재 절차 ID, stage가 화상 오디션이어야 한다.
+     * @param fileUrl        화상 오디션을 위한 참고 파일을 저장해 둔 URL
+     * @param announcementId 현재 공고 ID
+     * @param producerId     현재 공고를 작성한 제작사 ID
+     * @param user           현재 로그인 한 유저
      * @return 화상 오디션에 대한 세션 정보를 저장하는 데 성공한 경우
      * @throws NotFoundException        오디션의 현재 절차를 조회할 수 없을 때 발생하는 예외
      * @throws IllegalArgumentException 현재 절차의 stage가 화상 오디션이 아닐 때 발생하는 예외
@@ -155,6 +158,29 @@ public class AuditionController {
     ) throws NotFoundException, IllegalArgumentException {
         auditionService.saveSession(processId, fileUrl);
         return Response.create(HttpStatus.ACCEPTED, "화상 오디션을 위한 세션 정보를 저장했습니다.");
+    }
+
+    /**
+     * 프론트에서 넘겨주는 데이터의 한계로 한 명의 유저가 한 개의 배역에만 지원했다고 가정했습니다. 추후에 수정한다면 프론트에서 배역별로 일정을 받아야 하고, 배역별로 권한을 확인하는 방향으로 수정해야 합니다.
+     *
+     * @param processId      오디션의 현재 절차 ID, stage가 화상 오디션이어야 한다.
+     * @param bookRequestDto 오디션 시작 시간과 종료 시간을 전달하는 DTO
+     * @param announcementId 현재 공고 ID
+     * @param user           현재 로그인 한 유저
+     * @return 화상 오디션 예약에 성공한 경우 '201 CREATED' 반환
+     * @throws NotFoundException        오디션의 현재 절차를 조회할 수 없을 때 발생하는 예외
+     * @throws IllegalArgumentException 현재 절차의 stage가 화상 오디션이 아닐 때 발생하는 예외
+     */
+    @PostMapping("/on/{processId}/book")
+    @PreAuthorize("@checker.isLoginUser(#user) && @checker.hasPassedAuthority(#user, #announcementId, #processId)")
+    public ResponseEntity<Body> saveBookOfVolunteer(
+            @PathVariable Long processId,
+            @RequestBody BookRequestDto bookRequestDto,
+            @PathVariable Long announcementId,
+            @AuthenticationPrincipal PrincipalDetails user
+    ) throws NotFoundException, IllegalArgumentException {
+        auditionService.saveBookOfVolunteer(processId, bookRequestDto, user);
+        return Response.create(HttpStatus.CREATED, "지원자의 화상 오디션 일정을 예약했습니다.");
     }
 
 }
