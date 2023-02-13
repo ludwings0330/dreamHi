48894c4e0b2fbc6f52bd80e33ab6339ad9430054
DaWoon Lee <qkgk61@gmail.com>
2023-02-13 21:45:58 +0900

    :bug: Fix: LocalDateTime과 String 사이의 Convert 문제 해결
    @DateTimeFormat과 @JsonFormat을 적용하여 전환이 되도록 수정

--- back/Dream-Hi/src/main/java/com/elephant/dreamhi/model/dto/BookPeriod.java
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/model/dto/BookPeriod.java
@@ -1,6 +1,10 @@
 package com.elephant.dreamhi.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,9 +20,17 @@ import lombok.ToString;
 public class BookPeriod {
 
     @NotNull
+    @JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private LocalDate startDate;
 
     @NotNull
+    @JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private LocalDate endDate;
+
+    public BookPeriod(String startDate, String endDate) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd").localizedBy(Locale.KOREA);
+        this.startDate = LocalDate.parse(startDate, dateTimeFormatter);
+        this.endDate = LocalDate.parse(endDate, dateTimeFormatter);
+    }
 
 }
