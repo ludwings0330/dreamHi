4cd4b831d70f2d732e256f9521335d0b486330a8
이다운 <innocent61@naver.com>
2023-02-14 00:11:09 +0900

    :poop: Fix: userId에 적용한 JsonFormat을 삭제

--- back/Dream-Hi/src/main/java/com/elephant/dreamhi/model/dto/BookProducerDto.java
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/model/dto/BookProducerDto.java
@@ -1,7 +1,5 @@
 package com.elephant.dreamhi.model.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,7 +15,6 @@ import lombok.experimental.SuperBuilder;
 public class BookProducerDto extends BookResponseDto {
 
     @NotNull
-    @JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private Long userId;
 
     public BookProducerDto(Long id, String startTime, String endTime, Boolean reserved, Long userId) {
