1b7e960c1604c2070632ab30ffae79147f754ee0
JunHyxxn <ihrug000@gmail.com>
2023-02-14 00:17:26 +0900

    :sparkles: Feat: 오디션 일정 생성을 위한 RequestDTO 추가

---
+++ back/Dream-Hi/src/main/java/com/elephant/dreamhi/model/dto/AuditionCreateRequestDto.java
@@ -0,0 +1,24 @@
+package com.elephant.dreamhi.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class AuditionCreateRequestDto {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime startDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime endDate;
+
+}
