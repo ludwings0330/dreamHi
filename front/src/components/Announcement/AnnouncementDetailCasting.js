2c97b5f71020c287d72233bdff157c8817d00406
leeymk <leeymk11@gmail.com>
2023-02-15 05:51:10 +0900

    :sparkles: Feat: 프로필 제출 모달

--- front/src/components/Announcement/AnnouncementDetailCasting.js
+++ front/src/components/Announcement/AnnouncementDetailCasting.js
@@ -4,29 +4,24 @@ import { announcementListDetailCastingSelector } from 'recoil/announcement/annou
 
 function AnnouncementDetailCasting(props) {
   const { announcement } = props;
-  console.log('🤦‍♀️🤦‍♀️🤦‍♀️',announcement)
-  const CastingData = useRecoilValue(announcementListDetailCastingSelector(announcement.id))
+  console.log('🤦‍♀️🤦‍♀️🤦‍♀️', announcement);
+  const CastingData = useRecoilValue(announcementListDetailCastingSelector(announcement.id));
 
-  const data = CastingData
+  const data = CastingData;
 
   return (
     <div>
       {data.map(function (casting, index) {
-        console.log(casting)
+        console.log(casting);
         return (
-          <div className='announcement-post-container'>
-            <div className='announcement-content'> 배역 명 : {casting.name} </div>
-            <div className='announcement-content'> 성별 : {casting.gender} </div>
-            <div className='announcement-content'> 인원 : {casting.headcount} 명 </div>
-            <div className='announcement-content'> 기타 상세 : {casting.description} </div>
-
-
+          <div className="announcement-post-container">
+            <div className="announcement-content"> 배역 명 : {casting.name} </div>
+            <div className="announcement-content"> 성별 : {casting.gender} </div>
+            <div className="announcement-content"> 인원 : {casting.headcount} 명 </div>
+            <div className="announcement-content"> 기타 상세 : {casting.description} </div>
           </div>
         );
       })}
-
-
-
     </div>
   );
 }
