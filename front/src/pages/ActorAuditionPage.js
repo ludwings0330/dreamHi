5311940e4b1ab48fdf8bb3da9fec9e070ca4ccc0
JunHyxxn <ihrug000@gmail.com>
2023-02-16 01:08:57 +0900

    :sparkles: Feat: 예약 여부에 따라 페이지 내용 구성 변경

--- front/src/pages/ActorAuditionPage.js
+++ front/src/pages/ActorAuditionPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import dayjs from 'dayjs';
 import { Box, Container } from '../../node_modules/@mui/material/index';
 import Divider from '@mui/material/Divider';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
@@ -10,9 +10,13 @@ import { useRecoilValue } from 'recoil';
 import ActorTimeTable from 'components/Calendar/ActorTimeTable';
 import { announcementListDetailState } from 'recoil/announcement/announcementStore';
 import DateCalendar from 'components/Calendar/DateCalendar';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import { isBookedSelector } from '../recoil/book/bookStore';
 
 function ActorAuditionPage() {
   const announcementDetail = useRecoilValue(announcementListDetailState);
+  const isBooked = useRecoilValue(isBookedSelector());
 
   return (
     <Container
@@ -44,7 +48,34 @@ function ActorAuditionPage() {
             <Divider variant="fullWidth" sx={{ bgcolor: 'black' }} />
             <AccessTimeIcon sx={{ mt: 2, mb: 2, color: '#45b6fe' }} /> 시간 선택
             <Divider variant="fullWidth" sx={{ bgcolor: 'black' }} />
-            <ActorTimeTable />
+            {isBooked.reserved ? (
+              <Paper
+                elevation={8}
+                sx={{
+                  ml: 3,
+                  borderRadius: 3,
+                }}
+              >
+                <Box
+                  sx={{
+                    color: '#41424C',
+                  }}
+                >
+                  <Typography variant="h6" align="center" mt={2}>
+                    오디션 응시일 : {dayjs(isBooked.startDateTime).format('YYYY-MM-DD')}
+                    <br />
+                    시간 : {dayjs(isBooked.startDateTime).format('HH:mm')} ~{' '}
+                    {dayjs(isBooked.endDateTime).format('HH:mm')}
+                  </Typography>
+                  <Divider variant="fullWidth" mb={2} sx={{ bgcolor: 'black' }} />
+                  <Typography variant="subtitle1" align="center" mb={2}>
+                    오디션 응시 시간에 맞춰 5분 이내에 입장해주세요.
+                  </Typography>
+                </Box>
+              </Paper>
+            ) : (
+              <ActorTimeTable />
+            )}
           </Box>
         </Grid>
         <Divider orientation="vertical" flexItem variant="inset" sx={{ bgcolor: 'black' }} />
