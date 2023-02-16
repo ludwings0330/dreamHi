e5b254fd710f006499ca033a5f0daef37aa9ab66
ludwings0330 <ludwings0330@gmail.com>
2023-02-16 16:08:20 +0900

    :sparkles: Feat: 지원자 목록 관련 기능 구현

--- front/src/pages/CastingPage.js
+++ front/src/pages/CastingPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Layout from '../components/Common/MainLayout/Layout';
 import CastingRow from '../components/Casting/CastingRow';
+import {useLocation} from "react-router-dom";
 
 const CastingPage = () => {
+  const location = useLocation();
   return (
     <>
-      <CastingRow />
+      <h1>캐스팅 지원자 목록</h1>
+      <CastingRow announcementId={location.state.announcementId}/>
     </>
   );
 };
